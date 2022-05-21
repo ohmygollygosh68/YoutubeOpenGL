4b04fa64ada92971a7e04845a6fdc5c85eb3b242
ohmygollygosh68 <ohmygollygosh@outlook.com>
2022-05-21 17:27:15 +0100

    Exercise 1.1 change the equilateral triangle to a right triangle

--- Main.cpp
+++ Main.cpp
@@ -84,7 +84,8 @@ int main()
 	GLfloat vertices[] =
 	{
 		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
-		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
+		// 0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
+		0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner. Exercise 1.1 Change the equilateral triangle to a right triangle
 		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
 	};
 
