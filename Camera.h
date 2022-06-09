9aad893e1d9b590beb5d8fd744fb82aa5c31ae04
ohmygollygosh68 <ohmygollygosh@outlook.com>
2022-06-09 21:24:04 +0100

    Adjusted camera speed and sensitivity

--- Camera.h
+++ Camera.h
@@ -27,8 +27,8 @@ public:
 	int height;
 
 	// Adjust the speed of the camera and it's sensitivity when looking around
-	float speed = 0.01f;
-	float sensitivity = 100.0f;
+	float speed = 0.001f;		//0.1f
+	float sensitivity = 10.0f;  //100.0f
 
 	// Camera constructor to set up initial values
 	Camera(int width, int height, glm::vec3 position);
